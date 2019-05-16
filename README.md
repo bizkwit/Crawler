f753c49f2aeeb4464e335163d9c18d58ccdae3a4
dima <dima.shilchikov@gmail.com>
2019-05-16 15:56:53 -0400

    lookup finish

--- README.md
+++ README.md
@@ -3,4 +3,5 @@ A web crawler implemented using python:
 
     1 - Uses priority queue for each link it gathers
     2 - Uses miltuthreads to run crawlers in parallel
+    3 - Uses html.parser library for link parsing
     
--- lookup.py
+++ lookup.py
@@ -1,2 +1,26 @@
 from html.parser import HTMLParser
 from urllib import parse
+
+
+class LinkLookup(HTMLParser, homepage, current_page_url):
+
+    def __init__(self):
+        return super().__init__()
+        self.homepage = homepage
+        self.currentpage = current_page_url
+        self.links = set()
+
+    #method override
+    def handle_starttag(self, tag, attrs):
+        if tag=='a':
+            for (attribute, value) in attrs: #extract a tuple of att/value from a tag
+                if attribute == 'href':
+                    formatted_url = parse.urljoin(self.homepage, value)#if full url: do nothing. else combine with the homepage to make it full
+                    self.links.add(formatted_url)
+    #links getter
+    def get_links(self):
+        return self.links
+
+
+finder = LinkLookup()
+finder.feed('<html><head><title>Test</title></head><body><h1>Parse Me!</h1></body></html>')
