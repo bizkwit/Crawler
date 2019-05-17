0fc301334f1e4745c427f716c01513d648b8eb1a
dima <dima.shilchikov@gmail.com>
2019-05-16 20:07:11 -0400

    updated files

--- spider.py
+++ spider.py
@@ -6,7 +6,7 @@ from general import *
 class Spider:
 
     # Class variables (shared among all instances)
-    project_name = ''
+    project = ''
     homepage = ''
     domain_name = ''
     open_list_file = ''
@@ -14,12 +14,12 @@ class Spider:
     open_list_set = set()
     closed_list_set = set()
 
-    def __init__(self, project_name, homepage, domain_name):
-        Spider.project_name = project_name
+    def __init__(self, project, homepage, domain_name):
+        Spider.project = project
         Spider.homepage = homepage
         Spider.domain_name = domain_name
-        Spider.open_list_file = Spider.project_name + 'to visit.txt'
-        Spider.closed_list_file = Spider.project_name + 'visited.txt'
+        Spider.open_list_file = Spider.project + 'to visit.txt'
+        Spider.closed_list_file = Spider.project + 'visited.txt'
         self.boot()
         self.crawl('Initial spider', Spider.homepage) #happen once to initial the crawl
 
@@ -28,8 +28,8 @@ class Spider:
         '''
         Initiates the project and creates all the necessary files and folders for the spiders
         '''
-        create_project_dir(Spider.project_name)
-        create_data_file(Spider.project_name, Spider.homepage)
+        create_project_dir(Spider.project)
+        create_data_file(Spider.project, Spider.homepage)
         Spider.open_list_set = convert_to_set(Spider.open_list_file)
         Spider.closed_list_set = convert_to_set(Spider.closed_list_file)
 
