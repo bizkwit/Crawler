911ab583904d3f611f9203f645f21ebe3f801637
dima <dima.shilchikov@gmail.com>
2019-05-20 19:57:05 -0400

    fxed domain

--- domain.py
+++ domain.py
@@ -4,7 +4,7 @@ from urllib.parse import urlparse
 def get_domain_name(url):
     try:
         results = get_sub_domain_name(url).split('.')
-        return results[-2] +'.'+ results[-1]
+        return results[-3] +'.'+ results[-2] +'.'+ results[-1]
     except:
         return ''
 
--- main.py
+++ main.py
@@ -5,8 +5,9 @@ from domain import *
 from general import *
 
 
+
 PROJECT_NAME =  "Test"#input("What is the name of your project: ")
-HOMEPAGE = "http://titan.dcs.bbk.ac.uk/~kikpef01/testpage.html" #input("Please enter the website you want to run your spider on: ")
+HOMEPAGE = "bizkwit.github.io" #input("Please enter the website you want to run your spider on: ")
 DOMAIN = get_domain_name(HOMEPAGE)
 QUEUE_FILE = PROJECT_NAME+'/to_visit.txt'
 VISITED_FILE = PROJECT_NAME + '/visited.txt'
